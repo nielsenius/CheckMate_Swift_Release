c866f6f4341d011592be4449c4caf18757e607c5
Matthew <mqn@andrew.cmu.edu>
2014-10-08 15:42:34 -0400

    Added model to project.

---
+++ CheckMate/Model.swift
@@ -0,0 +1,73 @@
+//
+//  Model.swift
+//  CheckMate
+//
+//  Created by Matthew Nielsen on 10/7/14.
+//  Copyright (c) 2014 Matthew Nielsen. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    var percent: Float
+    var bill: String
+    var splits: Int
+    var custom: String
+    
+    let maxSplits: Int
+    let minSplits: Int
+    
+    var tip: String {
+        get {
+            var billFloat = (bill as NSString).floatValue
+            return String(format: "%.2f", billFloat * percent)
+        }
+    }
+    var total: String {
+        get {
+            var billFloat = (bill as NSString).floatValue
+            return String(format: "%.2f", billFloat + (billFloat * percent) / Float(splits))
+        }
+    }
+    
+    init() {
+        percent = 0.00
+        bill = "0"
+        splits = 1
+        custom = "0"
+        
+        maxSplits = 100
+        minSplits = 1
+    }
+    
+    func setPercent(tag: Int) {
+        switch tag {
+        case 16:
+            percent = 0.00
+        case 17:
+            percent = 0.10
+        case 18:
+            percent = 0.15
+        default:
+            percent = 0.20
+        }
+    }
+    
+    func setPercentFromCustom() {
+        percent = (custom as NSString).floatValue
+    }
+    
+    func addSplit() {
+        if splits < maxSplits {
+            splits += 1
+        }
+    }
+    
+    func subSplit() {
+        if splits > minSplits {
+            splits -= 1
+        }
+    }
+    
+}
--- CheckMate/ViewController.swift
+++ CheckMate/ViewController.swift
@@ -9,22 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //
+    // initialize constants
+    //
+    
+    func setConstants() {
+        let moneyColor = UIColor(red: 6 / 256.0, green: 92 / 256.0, blue: 39 / 256.0, alpha: 1)
+        let lightGrayColor = UIColor(red: 224 / 256.0, green: 224 / 256.0, blue: 224 / 256.0, alpha: 1)
+        let midGrayColor = UIColor(red: 192 / 256.0, green: 192 / 256.0, blue: 192 / 256.0, alpha: 1)
+        setCurrencySymbol()
+        setThousandsSeparator()
+    }
+    
+    func setCurrencySymbol() {
+        if let value = NSUserDefaults.standardUserDefaults().objectForKey("currency") as? String {
+            let sym = value
+        } else {
+            let sym = "$"
+        }
+    }
+    
+    func setThousandsSeparator() {
+        if let value = NSUserDefaults.standardUserDefaults().objectForKey("separators") as? Int {
+            let sep = value
+        } else {
+            let sep = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // do any additional setup after loading the view
+        setConstants()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    let moneyColor = UIColor(red: 6 / 256.0, green: 92 / 256.0, blue: 39 / 256.0, alpha: 1)
-    let lightGrayColor = UIColor(red: 224 / 256.0, green: 224 / 256.0, blue: 224 / 256.0, alpha: 1)
-    let midGrayColor = UIColor(red: 192 / 256.0, green: 192 / 256.0, blue: 192 / 256.0, alpha: 1)
-    
-    
 
 }
 
